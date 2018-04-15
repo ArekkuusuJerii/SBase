6c73eedba51da6cb86fe74d815ccc1e3169455b0
ArekkuusuJerii <edgargcx99@gmail.com>
2018-04-15 17:23:58 -0500

    DB Connection

---
+++ src/main/scala/connection/connection.scala
@@ -0,0 +1,22 @@
+package connection {
+
+  import java.sql.DriverManager
+
+  case class Info(ip: String, port: Int, database: String, user: String = _, password: String = _)
+
+  object Driver {
+    val MySQL: Driver = new Driver("jdbc:mysql://%1$s:%3$s/%2$s")
+    val SLQServer: Driver = new Driver("jdbc:sqlserver://%1$s:%2$s;databaseName=%3$s")
+  }
+
+  case class Driver(format: String, info: Info = _) {
+    def withInfo(info: Info): Driver = copy(info = info)
+
+    def copy(format: String = format, info: Info = info) = new Driver(format, info)
+
+    def open() = Option.apply(
+      DriverManager getConnection(String format(format, info.ip, info.port, info.database), info.user, info.password)
+    )
+  }
+
+}
